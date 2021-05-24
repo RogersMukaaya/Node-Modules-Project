8545bdbb6a5238ac1deeedbfe6e7d5226013e63d
rogers mukaaya <mukaayarogers7@gmail.com>
2021-05-24 13:08:33 +0300

    Access files dynamically

--- index.js
+++ index.js
@@ -35,7 +35,50 @@ const server = http.createServer((req, res) => {
 
     console.log(filePath);
 
-    res.end();
+    // Extension of file
+    let extname = path.extname(filePath);
+
+    let contentType = 'text/html';
+
+    // Check ext and set content type
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    // Read File
+    fs.readFile(filePath, (err, content) => {
+        if(err) {
+            if(err.code == 'ENOENT') {
+                // Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                })
+            } else {
+                // Soe server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            // Success
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    })
 });
 
 
